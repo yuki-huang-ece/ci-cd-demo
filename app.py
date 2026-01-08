d671e81e10a8f0cd5a2e04f096860a0abdd4dcdd
Huang Yuki <yuki.huang@ecloudvalley.com>
2026-01-08 09:16:51 +0800

    test ci pass

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
+# app.py
 def add(a, b):
     return a + b
+
+def subtract(a, b):
+    return a - b
--- test_app.py
+++ test_app.py
@@ -1,4 +1,10 @@
-from app import add
+# test_app.py
+from app import add, subtract
 
 def test_add():
-    assert add(1, 2) == 4
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 5) == -5
